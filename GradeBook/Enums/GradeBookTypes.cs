dc0351ea40cd010ff7e7f9514ef54dd4016771d1
Azan <M66953@u-ssi.net>
2019-12-11 15:50:36 +0100

    Done task 1

---
+++ GradeBook/Enums/GradeBookTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+  
+        public enum GradeBookTypes
+        {
+            Standard,
+            Ranked,
+            ESNU,
+            OneToFour,
+            SixPoint
+        }
+    
+}
