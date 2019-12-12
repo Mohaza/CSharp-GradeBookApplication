649e114ceef1dacf570de4c6a89a6fab6bd77b18
Azan <M66953@u-ssi.net>
2019-12-12 16:23:51 +0100

    New Task

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,30 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+           
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+           
+            switch (averageGrade)
+            {
+                case var d when d >= 80:
+                    return 'A';
+                case var d when d < 80 && d >= 60:
+                    return 'B';
+                case var d when d < 60 && d >= 40:
+                    return 'C';
+                case var d when d < 40 && d >= 20:
+                    return 'D';
+                default:
+                    return 'F';
+            }
+        }
+        
     }
 }
